456fec1b08b9386c3fb0b9a35ab54a49a6c18848
mslovett21 <Patrycja.Krawczuk80@myhunter.cuny.edu>
2018-02-19 16:56:49 -0500

    NYT spider West Virginia

---
+++ spiders/nyt_west_virginia.py
@@ -0,0 +1,38 @@
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name="NYT"
+    first_name ="first"
+    last_name ="last"
+    start_urls = [
+        'https://www.nytimes.com',
+          
+    ]
+
+    def parse(self,response):
+    	first_names = ["Don",       "Bo",    "Evan",    "Joe",    "Patrick",   "Jack",    "Paula Jean",   "Tom"]
+    	last_names = ["Blankenship","Copley","Jenkins", "Manchin", "Morrisey", "Newbrough","Swearengin",  "Willis" ]
+    	for i in range(len(first_names)): 
+    		candidate_link = "https://www.nytimes.com/search/" + first_names[i] + " " + last_names[i] + "/newest"
+    		self.first_name = first_names[i]
+    		self.last_name = last_names[i]
+    		yield scrapy.Request(candidate_link, callback=self.parse_the_candidate)
+
+    def parse_the_candidate(self, response):
+    	candidate_links=[]
+    	candidate_links=response.css('li.SearchResults-item--3k02W a::attr(href)').extract()
+    	for link in candidate_links:
+    		yield scrapy.Request(link, callback=self.parse_the_article)
+
+
+    def parse_the_article(self, response):
+    	yield {
+    	       'newspaper_name' : "NYT",
+    	       'candidate_name': self.first_name + " " + self.last_name,
+    	       'articles_date' :response.css('time ::text').extract_first(),
+    	       'articles_title' :response.css('h1.headline ::text').extract(),
+               'article_text' : response.css('.story-body-text ::text').extract(),
+            }
+
+
