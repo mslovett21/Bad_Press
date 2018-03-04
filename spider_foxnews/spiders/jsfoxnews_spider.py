81c78835e813d1d168e14a0eaf87d0c1aca1cf35
renat khalikov <renavoo@gmail.com>
2018-03-04 18:19:16 -0500

    fully scrapes foxnews, follows links and next pages

--- spider_foxnews/spiders/jsfoxnews_spider.py
+++ spider_foxnews/spiders/jsfoxnews_spider.py
@@ -1,37 +1,71 @@
 import scrapy
 from scrapy_splash import SplashRequest
 
-class QuotesSpider(scrapy.Spider):
-  name = "jsfoxnews"
-  first = "name"
-  last = "name"
-  start_urls = ["http://www.foxnews.com"]
+#first_names = ["Don",       "Bo",    "Evan"]#,    "Joe",    "Patrick",   "Jack",    "Paula Jean",   "Tom"]
+#last_names = ["Blankenship","Copley","Jenkins"]#, "Manchin", "Morrisey", "Newbrough","Swearengin",  "Willis" ]
 
-  def start_requests(self):
-    first_names = ["Don",       "Bo",    "Evan",    "Joe",    "Patrick",   "Jack",    "Paula Jean",   "Tom"]
-    last_names = ["Blankenship","Copley","Jenkins", "Manchin", "Morrisey", "Newbrough","Swearengin",  "Willis" ]
-    for i in range(len(first_names)):
-      candidate_link = "http://www.foxnews.com/search-results/search?q=" + first_names[i] + "+" + last_names[i]
-      self.first = first_names[i]
-      self.last = last_names[i]
-      yield SplashRequest(url=candidate_link, callback=self.parse,
+
+
+class QuotesSpider(scrapy.Spider):
+    name="jsfoxnews_v1"
+    start_urls = ["http://www.foxnews.com"]
+
+    def parse(self,response):
+      first_names = ["Don",       "Bo",    "Evan",    "Joe",    "Patrick",   "Jack",    "Paula Jean",   "Tom"]
+      last_names = ["Blankenship","Copley","Jenkins", "Manchin", "Morrisey", "Newbrough","Swearengin",  "Willis" ]
+      #first_names = ["Don","Joe"]
+      #last_names = ["Blankenship","Manchin"]
+      for i in range(len(first_names)):
+        #http://www.foxnews.com/search-results/search?q=don%20blankenship&ss=fn&start=0
+        candidate_link = "http://www.foxnews.com/search-results/search?q=" + first_names[i] + "%20" + last_names[i] + "&ss=fn&start=0"
+        yield SplashRequest(url=candidate_link, callback=self.parse_the_search_results,
         args={
           'wait': 5,
         }, endpoint='render.html')
 
-  def parse(self, response):
-    for q in response.css("div.search-directive.ng-scope"):
-      yield {
-        'newspaper_name' : "http://www.foxnews.com",
-        #'candidate_name': self.first + " " + self.last,
-        'articles_date': q.css("span.search-date.ng-binding::text").extract_first(),
-        'articles_title': q.css("a.ng-binding::text").extract_first(),
-        'article_text': q.css("p.ng-binding.ng-scope::text").extract_first()
-      }
-    #next_page = response.css('li.arrow.no-margin a::attr(href.ng-click)').extract_first()
-    #if next_page is not None:
-    #    next_page = response.urljoin(next_page)
-    #    yield SplashRequest(next_page, callback=self.parse,
-    #      args={
-    #        'wait': 1,
-    #      }, endpoint='render.html')
+    def parse_the_search_results(self, response):
+      number_of_pages = []
+      number_of_pages = response.css('a.ng-binding.ng-scope::text').extract()
+      print("Printing <number_of_pages>")
+      print(number_of_pages)
+      candidate_links=[]
+      candidate_links=response.css('a.ng-binding::attr(href)').extract()
+      for link in candidate_links:
+        if link:
+          yield SplashRequest(url=link, callback=self.parse_the_article,
+          args={
+            'wait': 10,
+          }, endpoint='render.html')
+      for page in number_of_pages:
+        page_format = int(page)
+        print(page_format-1)
+        next_page = response.url
+        next_page = next_page[:-1]
+        next_page = next_page + str(page_format-1) + "0"
+        print(next_page)
+        yield SplashRequest(url=next_page, callback=self.parse_next_pages,
+        args={
+          'wait': 5,
+        }, endpoint='render.html')
+
+    def parse_next_pages(self, response):
+      candidate_links=[]
+      candidate_links=response.css('a.ng-binding::attr(href)').extract()
+      for link in candidate_links:
+        if link:
+          yield SplashRequest(url=link, callback=self.parse_the_article,
+          args={
+            'wait': 10,
+          }, endpoint='render.html')
+
+    def parse_the_article(self, response):
+      if response.css("time::attr(datetime)").extract_first():
+        if response.css("div.article-text p::text").extract():
+          yield {
+           'newspaper_name': "foxnews",
+           'articles_date': response.css("time::attr(datetime)").extract_first(),
+           'articles_title': response.css('h1::text').extract_first(),
+           'article_text': response.css("div.article-text p::text").extract()
+          }
+
+
