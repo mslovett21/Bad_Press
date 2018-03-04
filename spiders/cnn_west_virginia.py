d002796a7f4fde5fff6c775f8f35e1d45b0b0f4f
mslovett21 <Patrycja.Krawczuk80@myhunter.cuny.edu>
2018-03-04 18:50:46 -0500

    CNN scraper

---
+++ spiders/cnn_west_virginia.py
@@ -0,0 +1,65 @@
+import scrapy
+from scrapy_splash import SplashRequest
+
+
+first_names = ["Don",       "Bo",       "Joe",    "Patrick",   "Jack",    "Paula Jean",   "Tom"]
+last_names = ["Blankenship", "Copley","Manchin", "Morrisey", "Newbrough","Swearengin",  "Willis" ]
+
+
+
+class QuotesSpider(scrapy.Spider):
+    name="CNN2"
+    start_urls = [
+        'https://www.cnn.com',
+          
+    ]
+
+    def parse(self, response):
+        for i in range(len(first_names)):
+            results_link="https://www.cnn.com/search/?size=10&q=" + first_names[i] + "%20" + last_names[i]
+            yield SplashRequest(results_link, callback=self.parse_results, meta={'splash' : {'endpoint' : 'render.html', 'args' : { 'wait' : 5.0 }}})
+
+    def parse_results(self,response):
+        number_of_pages = 1
+        response_str = str(response)
+        response_str = response_str.split("<200 ",1)[1]
+        response_str = response_str.split(">",1)[0]
+        string_number_results = response.css(".cnn-search__results-count ::text").extract()
+        if len(string_number_results) == 0:
+            number_of_pages=0
+        else:
+            string_pages = string_number_results[0].split("of ",1)[1]
+            string_pages = string_pages.split(" for",1)[0]
+            number_of_pages = int(string_pages)
+            number_of_pages = int(number_of_pages/10) + 1
+            print(number_of_pages)
+        if number_of_pages > 30:
+            number_of_pages = 30
+
+        k=-10
+        for j in range(number_of_pages):
+            k = k + 10
+            candidate_link = response_str+ "&from=" + str(k) + "&page=" + str(j)
+            print(candidate_link)
+            yield SplashRequest(candidate_link, callback=self.parse_the_candidate, meta={'splash' : {'endpoint' : 'render.html', 'args' : { 'wait' : 6.0 }}})
+
+    def parse_the_candidate(self, response):
+        candidate_links=[]
+        candidate_links=response.css('h3.cnn-search__result-headline a::attr(href)').extract()
+        for link in candidate_links:
+            link = "https:"+ link
+            yield SplashRequest(link, callback=self.parse_the_article, meta={'splash' : {'endpoint' : 'render.html', 'args' : { 'wait' : 3.0 }}})
+
+
+    def parse_the_article(self, response):
+        yield {
+               'newspaper_name' : "CNN",
+               'articles_date' :response.css('.update-time ::text').extract(),
+               'articles_title' :response.css('h1.pg-headline ::text').extract(),
+               'articles_text': response.css('.zn-body__paragraph ::text').extract()
+            }
+
+
+
+
+
