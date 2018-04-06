6e63e9bc4c35be6cd21e56d43d5d1b7e55f633bd
rpoon6 <ricky.poon6@gmail.com>
2018-04-06 14:21:23 -0400

    Customized data set for popGraph, resized to fit

--- BadPressWebsite/BPW/badpress/templates/badpress/js/popGraph.js
+++ BadPressWebsite/BPW/badpress/templates/badpress/js/popGraph.js
@@ -1,150 +1,53 @@
-var data = [ { label: "Data Set 1", 
-               x: [0, 1, 2, 3, 4], 
-               y: [0, 1, 2, 3, 4] }
-            ] ;
-var xy_chart = d3_xy_chart()
-    .width(960)
-    .height(500)
-    .xlabel("X Axis")
-    .ylabel("Y Axis") ;
-var svg = d3.select("body").append("svg")
-    .datum(data)
-    .call(xy_chart) ;
 
-function d3_xy_chart() {
-    var width = 640,  
-        height = 480, 
-        xlabel = "X Axis Label",
-        ylabel = "Y Axis Label" ;
-    
-    function chart(selection) {
-        selection.each(function(datasets) {
-            //
-            // Create the plot. 
-            //
-            var margin = {top: 20, right: 80, bottom: 30, left: 50}, 
-                innerwidth = width - margin.left - margin.right,
-                innerheight = height - margin.top - margin.bottom ;
-            
-            var x_scale = d3.scale.linear()
-                .range([0, innerwidth])
-                .domain([ d3.min(datasets, function(d) { return d3.min(d.x); }), 
-                          d3.max(datasets, function(d) { return d3.max(d.x); }) ]) ;
-            
-            var y_scale = d3.scale.linear()
-                .range([innerheight, 0])
-                .domain([ d3.min(datasets, function(d) { return d3.min(d.y); }),
-                          d3.max(datasets, function(d) { return d3.max(d.y); }) ]) ;
 
-            var color_scale = d3.scale.category10()
-                .domain(d3.range(datasets.length)) ;
+var svg = d3.select("#popChart"),
+    margin = {top: 20, right: 20, bottom: 30, left: 50},
+    width = +svg.attr("width") - margin.left - margin.right,
+    height = +svg.attr("height") - margin.top - margin.bottom,
+    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-            var x_axis = d3.svg.axis()
-                .scale(x_scale)
-                .orient("bottom") ;
+var parseTime = d3.timeParse("%d-%b-%y");
 
-            var y_axis = d3.svg.axis()
-                .scale(y_scale)
-                .orient("left") ;
+//binds range and domain to svg width and height
+var x = d3.scaleTime().rangeRound([0, width]);
+var y = d3.scaleLinear().rangeRound([height, 0]);
 
-            var x_grid = d3.svg.axis()
-                .scale(x_scale)
-                .orient("bottom")
-                .tickSize(-innerheight)
-                .tickFormat("") ;
+var line = d3.line()
+    .x(function(d) { return x(d.date); })
+    .y(function(d) { return y(d.close); });
 
-            var y_grid = d3.svg.axis()
-                .scale(y_scale)
-                .orient("left") 
-                .tickSize(-innerwidth)
-                .tickFormat("") ;
+//loads tsv data, hard coded
+d3.tsv("data.tsv", function(d) {
+  d.date = parseTime(d.date);
+  d.close = +d.close;
+  return d;
+}, function(error, data) {
+  if (error) throw error;
 
-            var draw_line = d3.svg.line()
-                .interpolate("basis")
-                .x(function(d) { return x_scale(d[0]); })
-                .y(function(d) { return y_scale(d[1]); }) ;
+  x.domain(d3.extent(data, function(d) { return d.date; }));
+  y.domain(d3.extent(data, function(d) { return d.close; }));
 
-            var svg = d3.select(this)
-                .attr("width", width)
-                .attr("height", height)
-                .append("g")
-                .attr("transform", "translate(" + margin.left + "," + margin.top + ")") ;
-            
-            svg.append("g")
-                .attr("class", "x grid")
-                .attr("transform", "translate(0," + innerheight + ")")
-                .call(x_grid) ;
 
-            svg.append("g")
-                .attr("class", "y grid")
-                .call(y_grid) ;
+//draws x and y axis, greys out axis
+  g.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x))
+      .selectAll("path")
+      .style("stroke", "grey");
 
-            svg.append("g")
-                .attr("class", "x axis")
-                .attr("transform", "translate(0," + innerheight + ")") 
-                .call(x_axis)
-                .append("text")
-                .attr("dy", "-.71em")
-                .attr("x", innerwidth)
-                .style("text-anchor", "end")
-                .text(xlabel) ;
-            
-            svg.append("g")
-                .attr("class", "y axis")
-                .call(y_axis)
-                .append("text")
-                .attr("transform", "rotate(-90)")
-                .attr("y", 6)
-                .attr("dy", "0.71em")
-                .style("text-anchor", "end")
-                .text(ylabel) ;
+  g.append("g")
+      .call(d3.axisLeft(y))
+      .selectAll("path")
+      .style("stroke", "grey");
 
-            var data_lines = svg.selectAll(".d3_xy_chart_line")
-                .data(datasets.map(function(d) {return d3.zip(d.x, d.y);}))
-                .enter().append("g")
-                .attr("class", "d3_xy_chart_line") ;
-            
-            data_lines.append("path")
-                .attr("class", "line")
-                .attr("d", function(d) {return draw_line(d); })
-                .attr("stroke", function(_, i) {return color_scale(i);}) ;
-            
-            data_lines.append("text")
-                .datum(function(d, i) { return {name: datasets[i].label, final: d[d.length-1]}; }) 
-                .attr("transform", function(d) { 
-                    return ( "translate(" + x_scale(d.final[0]) + "," + 
-                             y_scale(d.final[1]) + ")" ) ; })
-                .attr("x", 3)
-                .attr("dy", ".35em")
-                .attr("fill", function(_, i) { return color_scale(i); })
-                .text(function(d) { return d.name; }) ;
 
-        }) ;
-    }
-
-    chart.width = function(value) {
-        if (!arguments.length) return width;
-        width = value;
-        return chart;
-    };
-
-    chart.height = function(value) {
-        if (!arguments.length) return height;
-        height = value;
-        return chart;
-    };
-
-    chart.xlabel = function(value) {
-        if(!arguments.length) return xlabel ;
-        xlabel = value ;
-        return chart ;
-    } ;
-
-    chart.ylabel = function(value) {
-        if(!arguments.length) return ylabel ;
-        ylabel = value ;
-        return chart ;
-    } ;
-
-    return chart;
-}
+//draws f(x)
+  g.append("path")
+      .datum(data)
+      .attr("fill", "none")
+      .attr("stroke", "darkorchid")
+      .attr("stroke-linejoin", "round")
+      .attr("stroke-linecap", "round")
+      .attr("stroke-width", 2.0)
+      .attr("d", line);
+});
