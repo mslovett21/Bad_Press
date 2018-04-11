d366fe7937afb8ce1973d217acb6fb2982a5c5e8
renkv <renavoo@gmail.com>
2018-04-10 23:27:53 -0400

    updated admin file

--- BadPressWebsite/BPW/badpress/admin.py
+++ BadPressWebsite/BPW/badpress/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
 # Register your models here.
+'''
+from .models import Candidate, Article, Issue, Stateresults
+
+admin.site.register(Genre)
+
+# Define the admin class
+class CandidateAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name')
+    fields = ['first_name', 'last_name']
+
+# Register the admin class with the associated model
+admin.site.register(Candidate, CandidateAdmin)
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('newspaper', 'title', 'date')
+
+@admin.register(BookInstance)
+class IssueAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'info')
+        }),
+    )
+    '''
