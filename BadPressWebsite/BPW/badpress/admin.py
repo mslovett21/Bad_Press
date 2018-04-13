802317ce5b1b299cd2369c95df636836590abf25
Renat Khalikov <renatkhalikov@Renats-iMac.fios-router.home>
2018-04-12 21:18:16 -0400

    integrated sqllite added admin page

--- BadPressWebsite/BPW/badpress/admin.py
+++ BadPressWebsite/BPW/badpress/admin.py
@@ -1,29 +1,41 @@
 from django.contrib import admin
 
 # Register your models here.
-'''
-from .models import Candidate, Article, Issue, Stateresults
 
-admin.site.register(Genre)
+from .models import Source, State, Issue, Article, Candidate
 
+#admin.site.register(Source)
+#admin.site.register(State)
+#admin.site.register(Issue)
+#admin.site.register(Article)
+#admin.site.register(Candidate)
 # Define the admin class
 class CandidateAdmin(admin.ModelAdmin):
-    list_display = ('last_name', 'first_name')
-    fields = ['first_name', 'last_name']
+    list_display = ('last_name', 'first_name', 'party', 'state', 'date_of_birth')
+    list_filter = ('party', 'state')
 
 # Register the admin class with the associated model
 admin.site.register(Candidate, CandidateAdmin)
 
+# Register the Admin classes for Article using the decorator
+
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('newspaper', 'title', 'date')
+    list_display = ('title', 'candidate', 'state', 'issue', 'source', 'link')
 
-@admin.register(BookInstance)
+# Register the Admin classes for BookInstance using the decorator
+
+@admin.register(Issue) 
 class IssueAdmin(admin.ModelAdmin):
-    list_filter = ('status', 'due_back')
-    fieldsets = (
-        (None, {
-            'fields': ('name', 'info')
-        }),
-    )
-    '''
+    list_display = ('name', 'info', 'URL_logo')
+
+@admin.register(State)
+class StateAdmin(admin.ModelAdmin):
+    list_display = ('name', 'primaries_date', 'URL_logo')
+
+# Register the Admin classes for BookInstance using the decorator
+
+@admin.register(Source) 
+class SourceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'URL_logo')
+
