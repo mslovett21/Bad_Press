32b7743595b313297666e74e0baf95aca2b5d273
renkv <renavoo@gmail.com>
2018-04-24 14:55:13 -0400

    added links to state map

--- BadPressWebsite/BPW/badpress/urls.py
+++ BadPressWebsite/BPW/badpress/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     #path('', views.index, name='index'),
     #path('about/', views.about, name='about'),
-    path('stateresults/', views.CandidateList.as_view(), name='stateresults'),
+    #path('stateresults/', views.CandidateList.as_view(), name='stateresults'),
     #path('candidate/', views.candidate, name='candidate'),
     #path('candidate/<int:pk>', views.CandidateDetailView.as_view(), name='candidate-detail'),
     #url(r'profile/(?P<username>.+)/$', accountsViews.profile, name="profile")
@@ -13,7 +13,7 @@ urlpatterns = [
     #path('issue/', views.issue, name='issue'),
 
     url(r'^$', views.index, name="index"),
-    #url(r'stateresults', views.stateresults, name="stateresults"),
+    url(r'^stateresults/(?P<slug>[-\w]+)/$', views.state, name="state"),
     url(r'^candidate/(?P<id>\d+)/$', views.candidate, name="candidate"),
     url(r'^article/(?P<id>\d+)/$', views.article, name="article"),
     url(r'^issue/(?P<id>\d+)/$',views.issue, name="issue"),
