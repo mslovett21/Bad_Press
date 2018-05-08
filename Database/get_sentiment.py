e5bb07f2ffa388cb45b463dcdf9f03892785554f
sarahmathew <sarahannmathew@gmail.com>
2018-05-07 22:05:42 -0400

    generated all target csvs of the right form

--- Database/get_sentiment.py
+++ Database/get_sentiment.py
@@ -59,3 +59,48 @@ def get_sentiment(input_file, output_file, candidate_table):
 
     with open(output_file, 'w') as f:
         f.write(article.to_json(orient = "records"))
+
+def add_sentiment_to_candidate(input_file, candidate_file, output_file):
+    candidate_table = pd.read_json(candidate_file)
+    pd.options.mode.chained_assignment = None
+
+    issue_count = {}
+    for i in range(len(candidate_table)):
+        issue_count[candidate_table["id"][i]] = {1:{"summed":0,"count":0},2:{"summed":0,"count":0},3:{"summed":0,"count":0},4:{"summed":0,"count":0},5:{"summed":0,"count":0}}
+
+    all_data = pd.read_json(input_file)
+
+    for i in range(len(all_data)):
+        candidate_id = all_data["candidate_fk"][i]
+        category = all_data["issue"][i]
+        sentiment = all_data["sentiment_score"][i]
+
+        if category < 6 and category > 0:
+            issue_count[candidate_id][category]["summed"] += sentiment
+            issue_count[candidate_id][category]["count"] += 1
+
+    for k,v in issue_count.items():
+        for k1,v1 in v.items():
+            if v1["count"] == 0:
+                v1["count"] = 1
+
+    for i in range(len(candidate_table)):
+        candidate_id = candidate_table["id"][i]
+
+        average1 = issue_count[candidate_id][1]["summed"] / issue_count[candidate_id][1]["count"]
+        candidate_table["score_issue_1"][i] = int(average1)
+
+        average2 = issue_count[candidate_id][2]["summed"] / issue_count[candidate_id][2]["count"]
+        candidate_table["score_issue_2"][i] = int(average2)
+
+        average3 = issue_count[candidate_id][3]["summed"] / issue_count[candidate_id][3]["count"]
+        candidate_table["score_issue_3"][i] = int(average3)
+
+        average4 = issue_count[candidate_id][4]["summed"] / issue_count[candidate_id][4]["count"]
+        candidate_table["score_issue_4"][i] = int(average4)
+
+        average5 = issue_count[candidate_id][5]["summed"] / issue_count[candidate_id][5]["count"]
+        candidate_table["score_issue_5"][i] = int(average5)
+
+    with open(output_file, 'w') as f:
+        f.write(candidate_table.to_json(orient = "records"))
